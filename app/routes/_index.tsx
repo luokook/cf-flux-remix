45c36fd001e9ace315924c983e1615ba069f0192
luokook <130514200+luokook@users.noreply.github.com>
2025-01-02 18:31:42 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -32,7 +32,7 @@ export default function Index() {
   const { cfAiStatus, configStatus } = useLoaderData<typeof loader>();
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 via-green-500 to-blue-500">
-      <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-3xl shadow-2xl p-10 max-w-3xl w-full">
+      <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-3xl shadow-2xl p-10 max-w-2aaxl w-full">
         <h1 className="text-4xl font-extrabold text-white mb-8 text-center">CF Flux Remix</h1>
         <nav>
           <ul className="space-y-4">
@@ -92,37 +92,37 @@ export default function Index() {
           <h2 className="text-2xl font-bold mb-4">系统状态</h2>
           
           <p>CF-AI状态：
-             <div className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
+             <span className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
                  ${cfAiStatus === "已连接" ? "bg-green-700" : "bg-red-500"}`}>
                    {cfAiStatus}
-              </div>
+              </span>
           </p>
           
           <p>AI绘画模型：
-             <div className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
+             <span className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
                  ${configStatus.CUSTOMER_MODEL_MAP === "已设置" ? "bg-green-700" : "bg-red-500"}`}>
                {configStatus.CUSTOMER_MODEL_MAP}
-             </div>
+             </span>
             </p>
           
           <p>翻译模型：
-            <div className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 bg-indigo-500`}>
+            <span className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 bg-indigo-500`}>
               {configStatus.CF_TRANSLATE_MODEL}
-            </div>
-            </p>
+            </span>
+          </p>
           
           <p>API Key：
-            <div className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
+            <span className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
                  ${configStatus.API_KEY === "已设置" ? "bg-green-700" : "bg-red-500"}`}>
               {configStatus.API_KEY}
-            </div>
+            </span>
             </p>
           
           <p>CF账号：
-            <div className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
+            <span className={`px-1 text-center rounded-xl border-dashed border-2 border-white-800 
                  ${configStatus.CF_ACCOUNT_LIST === "已设置" ? "bg-green-700" : "bg-red-500"}`}>
               {configStatus.CF_ACCOUNT_LIST}
-            </div>
+            </span>
             </p>
           
         </div>
