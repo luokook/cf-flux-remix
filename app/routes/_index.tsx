e041f8e11a7b8a24839a6aa29e74b3b87379b9c7
luokook <130514200+luokook@users.noreply.github.com>
2025-01-02 21:23:33 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -88,17 +88,17 @@ export default function Index() {
           </ul>
         </nav>
 
-        <div className="relative mt-8 text-white text-center border-dashed border-3 border-white-800">
+        <div className="mt-8 text-white text-center border-dashed border-3 border-white-800">
           <h2 className="text-2xl font-bold mt-2 mb-3">系统状态</h2>
           
-          <div className="relative m-0.5">CF-AI状态：
+          <div className="flex-1 relative m-0.5">CF-AI状态：
              <span className={`relative p-1 text-center rounded-xl border-dashed border-2 border-white-800 
                  ${cfAiStatus === "已连接" ? "bg-green-700" : "bg-red-500"}`}>
                    {cfAiStatus}
               </span>
           </div>
           
-          <div className="relative m-0.5">AI绘画模型：
+          <div className="flex-1 relative m-0.5">AI绘画模型：
              <span className={`relative p-1 text-center rounded-xl border-dashed border-2 border-white-800 
                  ${configStatus.CUSTOMER_MODEL_MAP === "已设置" ? "bg-green-700" : "bg-red-500"}`}>
                {configStatus.CUSTOMER_MODEL_MAP}
