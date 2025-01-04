ac5f554c6e3f48cda1e4ea9decced4f7e83a1c92
luokook <130514200+luokook@users.noreply.github.com>
2025-01-04 14:10:47 +0800

    Update generate-image.tsx

--- app/routes/generate-image.tsx
+++ app/routes/generate-image.tsx
@@ -278,7 +278,7 @@ const handlepromptxmapChange = (e: ChangeEvent<HTMLSelectElement>) => {
               name="prompt"
               value={prompt}
               onChange={handlePromptChange}
-              className="w-full text-4xl  px-5 py-3 rounded-xl border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400 bg-white bg-opacity-20  font-bold text-white placeholder-white placeholder-opacity-70 transition duration-300 ease-in-out hover:bg-opacity-30" 
+              className="w-full text-2xl text-pretty px-5 py-3 rounded-xl border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-400 bg-white bg-opacity-20  font-bold text-white placeholder-white placeholder-opacity-70 transition duration-300 ease-in-out hover:bg-opacity-30" 
               rows="5"
               placeholder="受不了了！来一个富有创意的提示词吧..."
               required
