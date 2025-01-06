d7798561b69f0e1ff45cd8fd825794e507886faa
luokook <130514200+luokook@users.noreply.github.com>
2025-01-07 02:11:34 +0800

    Update generate-image.tsx

--- app/routes/generate-image.tsx
+++ app/routes/generate-image.tsx
@@ -82,20 +82,20 @@ const GenerateImage: FC = () => {
     setPromptxmap("");
   };
 
-  const promptx = [
-    "1024x1024 undefined",
-    "free 唯美",
+  const promptx = [random
+    "1024x1024,undefined",
+    "free,唯美,random",
     "毛茸茸 怪物，幼崽，4 爪，可爱，（雪+雪），超高分辨率，完美，杰作，氛围",
-     "随机一个迪士尼角色，产品工作室拍摄，完美，杰作，3d",
+     "随机,迪士尼角色，产品工作室拍摄，完美，杰作，3d",
       "猫和老鼠追逐，经典动画情节，3d渲染，电影特效",
       "(masterpiece:1.2), best quality,PIXIV,cozy animation scenes,scenery, building, sky, (low angle view:1.5)",
       "人工智能女孩的情感肖像全文，充满活力，超真实，详细，民族穿着，平静，白发，白皙的皮肤，宇宙大眼睛，https://s.mj.run/6EOJlsWCpyo https://s.mj.run/OKHH6Y8khD8 --chaos 60 --ar 9:16 --style raw --sref https://s.mj.run/7C6AG2__q_k --personalize anycl7a --stylize 1000 --v 6.1",
       "随机一个商业级设计图标",
     "gold dia de los muertos pendant, intricate 2d vector geometric, cutout shape pendant, blueprint frame lines sharp edges, svg vector style, product studio shoot",
       "浅紫A5封面，白色芙蓉浮雕，上面浮雕标题“艳”，头版右侧白色标签，正面视图，木刻印花风格，白底，彩色铅笔画小笔，极简主义。 --ar 34:47",
-      "随机一张cosplay",
-    "随机一张海景图",
-     "随机一张二次元图片",
+      "随机,cosplay",
+    "随机,海景图",
+     "随机,二次元",
      "3d fluffy llama, closeup cute and adorable, cute big circular reflective eyes, long fuzzy fur, Pixar render, unreal engine cinematic smooth, intricate detail, cinematic",
       "3d毛茸茸的美洲驼，可爱的特写，可爱的圆形反光大眼睛，长长的绒毛，皮克斯渲染，虚幻引擎电影流畅，复杂的细节，电影",
 "田园风光，欧洲郊野，欧洲风光",
@@ -105,21 +105,21 @@ const GenerateImage: FC = () => {
       "夜晚是一个漆黑、白雪皑皑的阿尔卑斯山村庄，乡村窗户上闪烁着淡淡的金光，周围是高耸而阴暗的山脉; 3D，不切实际。 --ar 9:16",
       "On a summer evening breeze, a lovely girl lies peacefully on the autumn lawn, feeling the gentle wind blowing, with autumn maple leaves falling in the distance.",
       "100mm 等距浮岛照片，超现实火山，错综复杂，细节丰富，behance，微观世界平滑，微距锐利聚焦，居中",
-    "随机一个动漫人物",
-    "随机一个皮克斯角色形象，3d，电影级效果",
+    "随机,动漫人物",
+    "随机,皮克斯角色形象，3d，电影级效果",
       "hologram of a wolf floating in space, a vibrant digital illustration, dribbble, quantum wavetracing, black background, behance hd",
       "一位美丽的白发仙女，长着翅膀，晚上坐在魔法森林中长满青苔的地面上，周围环绕着发光的萤火虫，举起魔杖施展魔法。艺术家詹姆斯·C·风格的丰富多彩的奇幻插图。克里斯滕森，插图的黄金时代，发光的，年长的领主，约翰·鲍尔凯·尼尔森埃德蒙·杜拉克，蒂姆·沃克，宾得克k1000，色彩鲜艳，深红色和紫色，以及绿色。 --ar 3:4 --personalize ioa83pi --v 6.1",
        "维多利亚风格的房间采用空灵、柔和的绿色和蓝色调色板，捕捉到一种褪色的宏伟感。一位头发飘逸的女子背对着观众站着，穿着一件与房间精致纹理融为一体的复古礼服。巨大的、超现实的花朵--玫瑰和百合花--充满了她周围的空间，它们的花瓣擦着墙壁和天花板。一只帝王般的白孔雀优雅地栖息在华丽的椅子上，为场景增添了梦幻般的神奇气质。柔和的漫光透过透明窗帘过滤，投射出柔和的阴影。 --ar 4:3 --s 50 --v 6.1 --style raw",
         "100mm photo of isometric floating island in the sky, surreal volcano, intricate, high detail, behance, microworlds smooth, macro sharp focus, centered",
        "小巧可爱的等距客厅，在一个剖开的盒子里，光线柔和流畅，色彩柔和，紫蓝配色，色彩柔和，100mm 镜头，3d blender 渲染",
-     "随机一张动漫图",
+     "随机,动漫",
     "春风得意马蹄疾，一日看尽长安花",
 "1 girl,Clothes in the shape of snowflake,render,technology, (best quality) (masterpiece), (highly in detailed), 4K,Official art, unit 8 k wallpaper, ultra detailed, masterpiece, best quality, extremely detailed,CG,low saturation, as style, line art",
 "迷人的城市夜景，灯火灿烂，明亮巨大的月亮",
 "bestquality, masterpiece, super details, fine fabrics, high detailed eyes and detailed face, comic, extremely fine and detailed, perfect details, 1girl, solo, long hair, bangs, rose pink eyes, long sleeves, frilly pastel dress, lace accessory, sweet smile, holding a pink macaron, cotton candy pink hair, hair ribbons, soft pink and white dress, fairy tale garden, pink flowers, balloons",
 "百舸争流，鹰击长空，鱼翔浅底，万类霜天竞自由",
 "comic,bestquality, masterpiece, super details, fine fabrics, highly detailed eyes and face, extremely fine and detailed, perfect details, 1 girl, solo, long hair, bangs, rosy cheeks, pearl hair clips, strawberry blonde tresses, strawberry-shaped stud earrings, sweet lolita-style dress with berry prints, holding a basket of fresh strawberries, whimsical garden setting, sunny and bright",
-     "随机一张风景图",
+     "随机,风景图",
     "毛茸茸 怪物，幼崽，萌宠，4爪，可爱，超高分辨率，完美，杰作，氛围，svg矢量风格，产品工作室拍摄",
     "金色亡灵节吊坠，复杂的2D矢量几何图形，剪切形状吊坠，蓝图框架线条锐利边缘，svg矢量风格，产品工作室拍摄", 
      "kawaii low poly panda character, 3d isometric render, white background, ambient occlusion, unity engine",
@@ -129,7 +129,7 @@ const GenerateImage: FC = () => {
     "baalbek temple of jupiter, in microworld render style",
     "木星的巴尔贝克神庙，微世界渲染风格",
      "随机宫崎骏风格漫画图片，风景，人物，建筑",
-   "随机一张建筑图，真实",
+   "随机,建筑,真实",
     "tiny cute ninja toy, standing character, soft smooth lighting, soft pastel colors, skottie young, 3d blender render, polycount, modular constructivism, pop surrealism, physically based rendering, square image",
    "微小可爱的等距瓷杯咖啡，柔光，色彩柔和，100 毫米镜头，3D Blender 渲染，polycount 趋势，模块化建构主义，蓝色背景，物理渲染，居中",
 "100mm photo of isometric floating island in the sky, surreal volcano, intricate, high detail, behance, microworlds smooth, macro sharp focus, centered",
@@ -140,14 +140,16 @@ const GenerateImage: FC = () => {
 "Tiny cute isometric temple, soft smooth lighting, soft colors, soft colors, 100mm lens, 3d blender render, trending on polycount, modular constructivism, blue blackground, physically based rendering, centered",
 "天空中等距浮岛的 100 毫米照片，超现实火山，复杂，高细节，behance，微观世界平滑，微距锐利焦点，居中",
 "随机一张名胜古迹图片，真实，高级摄影",
-"随机一部电影海报",
-"随机一种动物，真实动物，不要合成",
+"随机,电影海报",
+"随机,动物，真实，不要合成",
     "头戴花冠的黑人女孩面部特写，光滑柔嫩的皮肤，梦幻般的大眼睛，美丽精致的彩色头发，对称的动漫大眼睛，柔和的光线，细致的面部，由新海诚、stanley artgerm lau、wlop、rossdraws 制作，概念艺术，数字绘画，正对镜头",
 "2d ferocious lion head, vector illustration, angry eyes, football team emblem logo, 2d flat, centered",
 "卡哇伊低多边形熊猫角色，3D 等距渲染，白色背景，环境闭塞，unity 引擎",
  "金色长发的时间女神，身着飘逸的绿松石至蓝色渐变魔法长袍。她五官精致，双手各持一件魔法武器，每件武器上都有一个时钟吊坠。在她身后，代表时间的光环熠熠生辉，周围环绕着象征时间和空间魔法的漩涡能量。这个场景迷人而神秘，捕捉到了时间和空间的本质",
    "天空，繁星，宇宙，深邃",
-   "随机宇宙图",
+   "随机,宇宙",
+    "随机,夜景",
+    "随机,动物",
     "Tiny cute isometric porcelain cup of coffee, soft smooth lighting, with soft colors, 100mm lens, 3d blender render, trending on polycount, modular constructivism, blue background, physically based rendering, centered",
     "二维凶猛狮头，矢量插画，愤怒的眼神，足球队徽标志，二维平面，居中",
     "在夏日的晚风中，一个可爱的女孩安详地躺在秋天的草坪上，感受着轻柔的风，远处的枫叶飘落",
@@ -159,14 +161,17 @@ const GenerateImage: FC = () => {
        "复古风格油画，巴黎景观，1930年 --ar 3:2 --v 6.1 --s 50",
       "詹姆斯·C·克里斯滕森（James C Christensen）的油画是一幅大约1750年的著名科学家，穿着那个时代的上流社会服装，坐在古典建筑的休息室里，摆着他们的行业工具，爱德华·兰西尔（Edward Landseer）风格的戏剧性灯光 --s 200 --ar 5:8 --v 6.1",
       "充满活力的场景捕捉了金·琼斯（Kim Jones）设想的迪奥2029年春季系列的精髓，展现了传统与颠覆之间的平衡，一位20岁的时尚达人散发着迪奥模特的优雅，同时展示了2029年的未来派服装，她快乐的表情为时尚的办公室增添了光彩，一只大鹿和异想天开的动物增强了视觉叙事，使用尼康Z9无反光镜数码相机拍摄，85毫米镜头，f/1.8,16 K分辨率， --ar 9：16 --stylize 500 --v 6.1",
-       "随机一个logo设计，现代感强，商业品质",
+       "随机,logo,现代感强，商业品质",
       "Tiny cute isometric living room in a cutaway box, soft smooth lighting, soft colors, purple and blue color scheme, soft colors, 100mm lens, 3d blender render",
        "地球爆炸，3d风格，世界末日",
        "C4D 3D渲染，Q版卡通风格，全身，拟人化感，一个可爱的美国小男孩，身上穿着红紫色运动衫，里面穿着黑紫色运动裤，和一群外星小怪物玩恐怖游戏，怪物头在空中弹跳，设计特点：Pop Mart盲盒玩具、鲜艳的色彩、明亮的图像、迪士尼皮克斯趋势、电影灯光、精湛的细节、高分辨率、最佳的细节 --ar 9：16",
      "充满活力的三层维多利亚式房屋 --ar 16:9",
       "火影忍者，鸣人，佐助，产品工作室拍摄，完美，杰作，氛围，svg矢量风格，3d风格，随机背景，其他随机火影人物",
-      "随机一张头像",
-    "Furry:: 1.3 monster, cub, 4 claws, cute, (snow+snow), super high resolution, perfection, masterpiece, atmosphere",
+      "随机,头像",
+    "随机,汉字",
+    "随机,人脸",
+       "random,图标",
+       "Furry:: 1.3 monster, cub, 4 claws, cute, (snow+snow), super high resolution, perfection, masterpiece, atmosphere",
        "皮卡丘在沙滩上晒日光浴，卡通，3d风格",
          "江上孤舟，远处青山，中国水墨画，飞鸟高飞"
      ];
@@ -232,8 +237,8 @@ const testCfAiConnection = async function(){
   const handlepromptfanyi = async function(){
     //const result = await testCfAiConnection();
     const prompt1 = document.getElementById("prompt").value;
-    setPrompt("抱歉，该功能暂时未上线……");
-    setPrompt(size+actionData.translatedPrompt);
+    //setPrompt("抱歉，该功能暂时未上线……");
+    setPrompt(actionData.translatedPrompt);
      
    };
 
